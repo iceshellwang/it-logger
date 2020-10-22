17fe6f221c52a63b74009a1c840328d02b36c744
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-10-22 23:56:12 +1100

    add searchBar component

---
+++ src/components/layout/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const SearchBar = () => {
+    return (
+        <nav>
+            <div className='nav-wrapper'>
+                <form>
+                    <div>
+                        <input />
+
+                        <label>
+                            <i>
+                                search
+           </i>
+                        </label>
+                        <i>close</i>
+                    </div>
+                </form>
+            </div>
+        </nav>
+    )
+}
+export default SearchBar
